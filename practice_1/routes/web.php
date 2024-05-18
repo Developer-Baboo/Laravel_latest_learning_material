e677a65c1b85cd724c0ae578e42d0a2629498c4c
Developer Baboo <babooheerani@gamil.com>
2024-05-17 17:19:25 -0700

    Laravel Resource controller

--- practice_1/routes/web.php
+++ practice_1/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\customValidation;
+use App\Http\Controllers\Comments;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\customValidation;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\ResourceController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -26,10 +28,34 @@ use App\Http\Controllers\LecturerController;
     return view('addUser');
 }); */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('custom_validation');
-});
+}); */
 
 
 // Route::post('/add', [UserController::class, 'addUser'])->name('addUser');
-Route::post('/custom_validation', [customValidation::class, 'custom_validation'])->name('custom_validation');
+// Route::post('/custom_validation', [customValidation::class, 'custom_validation'])->name('custom_validation');
+
+
+
+// ==============================================================
+//Laravel Resource controller
+// below will create 7 methods and their routes
+Route::resource('users', ResourceController::class);
+// ==============================================================
+
+// ==============================================================
+// below will create only specific methods
+Route::resource('users', ResourceController::class)->only([
+    'create' => 'update', 'show'
+]);
+// ==============================================================
+// below will skipps only specific methods
+Route::resource('users', ResourceController::class)->except([
+    'create' => 'update', 'show'
+]);
+// ==============================================================
+
+
+Route::resource('users', UserController::class);
+Route::resource('users.comments', Comments::class);
